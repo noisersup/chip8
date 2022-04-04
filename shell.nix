19edff6e27ff33e5f13a4bf3bd010568d74ba48f
noisersup <patryk@kwiatek.xyz>
2022-04-04 14:36:14 +0200

    added nix file

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+    xorgLibs = with pkgs.xorg;[
+        libXi
+        libXxf86vm
+        libX11.dev
+        libXft
+        libXcursor
+        libXinerama
+        libXrandr
+        libXrender
+        xorgproto
+    ];
+in
+pkgs.mkShell{
+    buildInputs = with pkgs; [
+        glfw
+
+        pkg-config
+        libcap go gcc
+ libGLU       libGL
+        xorgLibs
+    ];
+}
